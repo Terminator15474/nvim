55abca5ffe94282f7ea9e1f3bdc22533d22d1090
Terminator15474 <jandommert8@gmail.com>
2025-01-12 22:42:02 +0100

    added colorful-menu plugin and played around lsp config

--- lua/lazy/lazy_specs/completion.lua
+++ lua/lazy/lazy_specs/completion.lua
@@ -1,75 +1,120 @@
 return {
-	---@type blink.cmp.Config
+	{
+		"xzbdmw/colorful-menu.nvim",
+		config = function()
+			-- You don't need to set these options.
+			require("colorful-menu").setup({
+				ls = {
+					lua_ls = {
+						-- Maybe you want to dim arguments a bit.
+						arguments_hl = "@comment",
+					},
+					gopls = {
+						-- When true, label for field and variable will format like "foo: Foo"
+						-- instead of go's original syntax "foo Foo".
+						add_colon_before_type = false,
+					},
+					["rust-analyzer"] = {
+						-- Such as (as Iterator), (use std::io).
+						extra_info_hl = "@comment",
+					},
+					-- If true, try to highlight "not supported" languages.
+					fallback = true,
+				},
+				-- If the built-in logic fails to find a suitable highlight group,
+				-- this highlight is applied to the label.
+				fallback_highlight = "@variable",
+				-- If provided, the plugin truncates the final displayed text to
+				-- this width (measured in display cells). Any highlights that extend
+				-- beyond the truncation point are ignored. Default 60.
+				max_width = 60,
+			})
+		end,
+		lazy = false,
+		priotity = 1000
+	},
 	{
 		'saghen/blink.cmp',
 		dependencies = 'rafamadriz/friendly-snippets',
-		version = 'v0.*',
+		version = 'v0.9.2',
 
-		opts = {
-			keymap = {
-				preset = 'super-tab',
-				['<UP>'] = { 'select_prev', 'fallback' },
-				['<DOWN>'] = { 'select_next', 'fallback' },
-				['<C-space>'] = { function(cmp)
-					cmp.show({})
-				end },
-			},
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = 'mono',
-			},
-			sources = {
-				default = { 'lsp', 'lazydev', 'path', 'buffer' },
-				providers = {
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						score_offset = 100,
-					},
+		config = function()
+			---@type blink.cmp.Config
+			local opts = {
+				keymap = {
+					preset = 'super-tab',
+					['<UP>'] = { 'select_prev', 'fallback' },
+					['<DOWN>'] = { 'select_next', 'fallback' },
+					['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+					['<PageUp>'] = { 'scroll_documentation_up' },
+					['<PageDown>'] = { 'scroll_documentation_down' }
 				},
-			},
-			completion = {
-				accept = { auto_brackets = { enabled = true } },
-				trigger = {
-					prefetch_on_insert = true,
+				appearance = {
+					use_nvim_cmp_as_default = true,
+					nerd_font_variant = 'mono',
 				},
-				menu = {
-					draw = {
-						treesitter = {
-							'lsp',
-						}
-					}
-				},
-				documentation = {
-					auto_show = true,
-					-- Delay before showing the documentation window
-					auto_show_delay_ms = 100,
-					-- Delay before updating the documentation window when selecting a new item,
-					-- while an existing item is still visible
-					update_delay_ms = 50,
-					-- Whether to use treesitter highlighting, disable if you run into performance issues
-					treesitter_highlighting = true,
-					window = {
-						min_width = 10,
-						max_width = 80,
-						max_height = 25,
-						border = 'padded',
-						winblend = 0,
-						winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc',
-						-- Note that the gutter will be disabled when border ~= 'none'
-						scrollbar = true,
-						-- Which directions to show the documentation window,
-						-- for each of the possible menu window directions,
-						-- falling back to the next direction when there's not enough space
-						direction_priority = {
-							menu_north = { 'e', 'w', 'n', 's' },
-							menu_south = { 'e', 'w', 's', 'n' },
+				sources = {
+					default = { 'lsp', 'lazydev', 'path', 'buffer' },
+					providers = {
+						lazydev = {
+							name = "LazyDev",
+							module = "lazydev.integrations.blink",
+							score_offset = 100,
 						},
 					},
-				}
-			},
-			signature = { enabled = true, },
-		},
+				},
+				completion = {
+					accept = { auto_brackets = { enabled = true } },
+					trigger = {
+						prefetch_on_insert = true,
+					},
+					menu = {
+						draw = {
+							columns = { { "kind_icon" }, { "label", gap = 1 } },
+							components = {
+								label = {
+									text = require("colorful-menu").blink_completion_text,
+									highlight = require("colorful-menu").blink_completion_highlight,
+								},
+							},
+							treesitter = {
+								'lsp',
+							}
+						}
+					},
+					documentation = {
+						auto_show = true,
+						-- Delay before showing the documentation window
+						auto_show_delay_ms = 100,
+						-- Delay before updating the documentation window when selecting a new item,
+						-- while an existing item is still visible
+						update_delay_ms = 50,
+						-- Whether to use treesitter highlighting, disable if you run into performance issues
+						treesitter_highlighting = true,
+						window = {
+							min_width = 10,
+							max_width = 80,
+							max_height = 25,
+							border = 'padded',
+							winblend = 0,
+							winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc',
+							-- Note that the gutter will be disabled when border ~= 'none'
+							scrollbar = true,
+							-- Which directions to show the documentation window,
+							-- for each of the possible menu window directions,
+							-- falling back to the next direction when there's not enough space
+							direction_priority = {
+								menu_north = { 'e', 'w', 'n', 's' },
+								menu_south = { 'e', 'w', 's', 'n' },
+							},
+						},
+					}
+				},
+				signature = { enabled = true, },
+			}
+
+			require("blink.cmp").setup(opts)
+		end
 	},
 	{
 		'echasnovski/mini.pairs',
--- lua/lazy/lazy_specs/lsp.lua
+++ lua/lazy/lazy_specs/lsp.lua
@@ -1,37 +1,40 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		{
-			"folke/lazydev.nvim",
-			ft = "lua", -- only load on lua files
-			opts = {
-				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
 				},
 			},
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"saghen/blink.cmp",
 		},
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"saghen/blink.cmp",
-	},
-	config = function()
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		require("mason").setup {}
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"rust_analyzer",
-				"gopls",
-			},
-			handlers = {
-				function(server)
-					require("lspconfig")[server].setup {
-						capabilities = capabilities
-					}
-				end,
+		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			require("mason").setup {}
+			require("mason-lspconfig").setup {
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"rust_analyzer"
+				},
+				automatic_installation = true,
+				handlers = {
+					function(server)
+						require("lspconfig")[server].setup {
+							capabilities = capabilities
+						}
+					end,
+				}
 			}
-		})
-	end
+		end
+	},
 }
